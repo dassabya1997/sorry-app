d197599988bba7c43be9d6114d3631541068eb5e
Sabyasachi Das <sabyasachidas@Sabyasachis-MacBook-Pro.local>
2024-08-13 22:12:34 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SorryComponent } from './sorry/sorry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SorryComponent
   ],
   imports: [
     BrowserModule,
